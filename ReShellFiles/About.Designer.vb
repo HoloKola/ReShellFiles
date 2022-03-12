5ac3b1a07a67f841cf5aaf4b111a658a3af8b933
HoloKola <84939011+HoloKola@users.noreply.github.com>
2022-03-12 03:00:52 +0100

    Added MIT license copyright notice

--- ReShellFiles/About.Designer.vb
+++ ReShellFiles/About.Designer.vb
@@ -26,6 +26,7 @@ Partial Class About
         Me.OkButton = New System.Windows.Forms.Button()
         Me.AboutLabel = New System.Windows.Forms.Label()
         Me.AppInfo = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         Me.SuspendLayout()
         '
         'OkButton
@@ -57,12 +58,23 @@ Partial Class About
         Me.AppInfo.Text = "Version 0.4.0" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ReShell Files is the File Manager included with the " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "ReShell De" &
     "sktop Environment. It is free and open source."
         '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(20, 129)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(133, 15)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Show copyright notice"
+        '
         'About
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(434, 302)
+        Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.AppInfo)
         Me.Controls.Add(Me.AboutLabel)
         Me.Controls.Add(Me.OkButton)
@@ -85,4 +97,5 @@ Partial Class About
     Friend WithEvents OkButton As Button
     Friend WithEvents AboutLabel As Label
     Friend WithEvents AppInfo As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
 End Class
